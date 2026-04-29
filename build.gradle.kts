57cf172c33294259f239c2b4cdb5e2c3f78589bf
Melody Belle <melody@meluhdy.dev>
2026-04-29 14:58:50 -0400

    A few build.gradle.kts fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     kotlin("jvm") version "2.4.0-Beta1"
     id("com.gradleup.shadow") version "9.4.1"
-    id("xyz.jpenilla.run-paper") version "3.0.2"
-    kotlin("plugin.serialization") version "2.1.20"
+    id("dev.meluhdy.multi-module") version "1.0.0"
+    kotlin("plugin.serialization") version "2.4.0-Beta1"
     `maven-publish`
 }
 
@@ -21,18 +21,10 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
-    compileOnly("dev.meluhdy:melodia:1.0-SNAPSHOT")
-    compileOnly("dev.meluhdy:scoville:1.0-SNAPSHOT")
-}
-
-tasks {
-    runServer {
-        // Configure the Minecraft version for our task.
-        // This is the only required configuration besides applying the plugin.
-        // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("1.21")
-    }
+    multiModule.tryMultiModule(":Melodia", "dev.meluhdy:melodia:1.0-SNAPSHOT")
+    multiModule.tryMultiModule(":ScovilleCore", "dev.meluhdy:scoville-core:1.0-SNAPSHOT")
 }
 
 val targetJavaVersion = 25
